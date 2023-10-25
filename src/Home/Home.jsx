bf4de73471244e70b1be1d5dcb465864fc1c5777
cjhojan416 <cjhojan416@gmail.com>
2023-10-25 12:09:10 -0500

    se crea barberia

---
+++ src/Home/Home.jsx
@@ -0,0 +1,16 @@
+import './Home.css'
+import { Menu } from '../Menu/Menu'
+export function Home() {
+    return (
+        <>
+        <header>
+            <Menu></Menu>
+        </header>
+            <section className='banner'>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate rem, quod blanditiis dolorum quas sapiente excepturi officiis quis modi soluta iste at, vel officia voluptatibus earum sit, in quisquam commodi.
+                </p>
+            </section>
+        </>
+    )
+}
